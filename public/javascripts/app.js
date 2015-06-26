f65be896279ca550c7f7bc02ab9331e8530bde1a
jamesstaub <james.staub@gmail.com>
2015-06-26 10:30:42 -0400

    developing jquery interface for creating new survey

--- public/javascripts/app.js
+++ public/javascripts/app.js
@@ -11,10 +11,10 @@ var questionCount = 0;
 //         <span class="caret"></span> \
 //       </button> \
 //     <ul class="dropdown-menu" aria-labelledby="submit-new-question"> \
-//       <li><a class="choose-answer-format" data-answer-format="vertical">Vertical </a></li> \
-//       <li><a class="choose-answer-format" data-answer-format="horizontal">Horizontal</a></li> \
-//       <li><a class="choose-answer-format" data-answer-format="range">Range Slider</a></li> \
-//       <li><a class="choose-answer-format" data-answer-format="text">Text Area</a></li> \
+//       <li><a class="choose-answer-type" data-answer-type="vertical">Vertical </a></li> \
+//       <li><a class="choose-answer-type" data-answer-type="horizontal">Horizontal</a></li> \
+//       <li><a class="choose-answer-type" data-answer-type="range">Range Slider</a></li> \
+//       <li><a class="choose-answer-type" data-answer-type="text">Text Area</a></li> \
 //     </ul> \
 //   </div> \
 // </div> \
@@ -24,43 +24,41 @@ var questionCount = 0;
 var templates = {
     question: function(id){
       return' \
-        <div data-question-id="'+ id +'" class="create-question"> \
+        <div class="create-question"> \
           <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button> \
             <div class="form-group"> \
               <input type="text" class="create-question-title form-control" placeholder="Enter a question"> \
             </div> \
-          <div class="dropdown"> \
-              <button class="btn btn-default dropdown-toggle" type="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded="true"> \
-                Answer Format \
-                <span class="caret"></span> \
-              </button> \
-            <ul class="dropdown-menu" aria-labelledby="submit-new-question"> \
-              <li><a class="choose-answer-format" data-answer-format="vertical">Vertical </a></li> \
-              <li><a class="choose-answer-format" data-answer-format="horizontal">Horizontal</a></li> \
-              <li><a class="choose-answer-format" data-answer-format="range">Range Slider</a></li> \
-              <li><a class="choose-answer-format" data-answer-format="text">Text Area</a></li> \
+            <label>Answer Format</label> \
+            <ul class="nav nav-tabs answer-type-list"> \
+              <li role="presentation"><a href="#" class="choose-answer-type" data-answer-type="multiple">Multiple Choice</a></li> \
+              <li role="presentation"><a href="#" class="choose-answer-type" data-answer-type="range">Range Slider</a></li> \
+              <li role="presentation"><a href="#" class="choose-answer-type" data-answer-type="text" >Text Field</a></li> \
             </ul> \
-          </div> \
           <div data-question-id="'+id+'" class="answers-container"></div> \
         </div> \
           ';
         },
 
-        verticalAnswer: function(){
-          return ' \
-          <div class="create-answer"></div> \
-          <button type="button" class="close" aria-label="Close"><span aria-hidden="true">&times;</span></button>';
+
+
+        answer: function(id) {
+          var answerCreator = '';
+          console.log(id);
+          if(id === 'multiple'){
+            answerCreator = '<div class="form-group answer-creator multi-settings"><input type="text" class="create-answer form-control" placeholder="enter an answer choice"></div>';
+          }else if(id === 'range'){
+            answerCreator = '<div class="form-group answer-creator range-settings"><p>Users rate your question on a scale from </p><input type="number" class="create-answer form-control"  min="0" max="1"><span> to </span><input type="number" class="create-answer form-control"  min="1" max="100"></div>'
+          }else if(id === 'text'){
+            answerCreator = '<div class="answer-creator"><span>Users will be given a text field to leave an answer</span></div>';
+          }
+         return answerCreator;
         }
+
+
+
   }
 
-var newVertical = ' \
- <div class="form-group"> \
-      <input type="text" class="create-answer form-control" placeholder="Enter an answer"> \
-    </div> \
-';
-var newHorizontal = '';
-var newRange = '';
-var newText = ''
 
 // var newQuestion = {
 //     question : [{answrs}, {answrs}]
@@ -80,16 +78,18 @@ $(document).on('click', '.add-new-question', function(){
     $('.questions-container').append(templates.question(questionCount));
 });
 
-$(document).on('click', '.choose-answer-format', function(){
-    questionCount++
-    console.log(questionCount)
-    // questionCount = questionCount++
-    // $('.answers-container').append(templates.question(questionCount));
-});
+$(document).on('click', '.choose-answer-type', function(){
+    var thisType = $(this).data('answer-type');
+    // $('.answers-container').append(templates.answer(thisType));
+
+    $('.answers-container').html('');
+    $('.answers-container').append(templates.answer(thisType));
+    $('.answer-type-list li').removeClass('active');
+    $(this).parent().addClass('active');
+
+    if(thisType === 'multiple')
 
 
-$('.choose-answer-format').on('click', function(){
-  $(this).data('answer-format')
 });
 
 
