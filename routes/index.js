f649a0bb79037b3a73af5af85ffe4b05cc0f4c64
jamesstaub <james.staub@gmail.com>
2015-06-30 11:33:55 -0400

    index page update

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,31 @@
 var express = require('express');
 var router = express.Router();
+var bodyParser = require('body-parser');
+var jsonParser = bodyParser.json();
+var Survey = require('../models/surveys');
 
-/* GET home page. */
+// /* GET home page. */
+// router.get('/', function(req, res) {
+//   res.render('index', { title: 'Survey Says!?' });
+// }
+
+
+
+router.use(bodyParser.json({
+  extended: true
+}));
+router.use(bodyParser.urlencoded({
+  extended: false
+}));
+router.use(bodyParser.json());
+
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Survey Says!?' });
+  Survey.find({}, function(err, surveys, count) {
+    res.render('index', {
+      surveys: surveys
+    });
+  });
 });
+
 
 module.exports = router;
